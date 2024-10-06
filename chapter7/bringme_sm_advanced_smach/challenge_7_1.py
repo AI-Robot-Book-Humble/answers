d0bb35e1e838e3a62567026ffdec55f9637a50b0
MrKeith99 <kvalentincardenas@hotmail.com>
2024-10-06 19:29:38 +0900

    [UPD] challenge7.1 is deleted, challenge7.2 is renamed

--- chapter7/bringme_sm_advanced_smach/challenge_7_1.py
+++ chapter7/bringme_sm_advanced_smach/challenge_7_1.py
@@ -1,133 +1,81 @@
-# ファイル名：sample_sm.py
-
 import rclpy
-from rclpy.node import Node
+import rclpy.node
+from airobot_interfaces.srv import StringCommand
 
-import smach
-
-import random
-
-# define state Search
-class Search(smach.State):
-    def __init__(self, _node):
-        smach.State.__init__(self, outcomes=['succeeded','finished'])
-        self.counter = 0
-        self.logger = _node.get_logger()
-
-    def execute(self, userdata):
-        self.logger.info('I am searching.')
-        if self.counter < 3:
-            self.logger.info('Got a sweet.')
-            self.counter += 1
-            return 'succeeded'
-        else:
-            self.logger.info('I am full.')
-            return 'finished'
-
-# define state Grasp
-class Grasp(smach.State):
-    def __init__(self, _node):
-        smach.State.__init__(self, outcomes=['succeeded','failed'])
-        self.logger = _node.get_logger()
-
-    def execute(self, userdata):
-        self.logger.info('I graspped a food.')
-
-        prob = random.random()
-
-        if prob >= 0.5:
-            return 'succeeded'
-
-        return 'failed'
-
-# define state Eat
-class Eat(smach.State):
-    def __init__(self, _node):
-        smach.State.__init__(self, outcomes=['done'])
-        self.logger = _node.get_logger()
-
-    def execute(self, userdata):
-        self.logger.info('I am eating.')
-        return 'done'
+from gtts import gTTS
+import speech_recognition as sr
+from io import BytesIO
+from mpg123 import Mpg123, Out123
 
 
-class StateMachine(Node):
+class SpeechService(rclpy.node.Node):
     def __init__(self):
-        super().__init__('state_machine')
+        super().__init__('speech_service')
 
-    def execute(self):
-        # Create a SMACH state machine
-        sm = smach.StateMachine(outcomes=['end'])
-        # Open the container
-        with sm:
-            # Add states to the container
-            smach.StateMachine.add(
-                'SEARCH', Search(self),
-                transitions={'succeeded': 'GRASP', 'finished': 'end'})
-            smach.StateMachine.add(
-                'GRASP', Grasp(self),
-                transitions={'succeeded': 'EAT', 'failed': 'GRASP'})
-            smach.StateMachine.add(
-                'EAT', Eat(self),
-                transitions={'done': 'SEARCH'})
+        self.get_logger().info('音声サーバーを起動しました')
 
-        # Execute SMACH plan
-        outcome = sm.execute()
-        self.get_logger().info(f'outcom: {outcome}')
+        self.init_rec = sr.Recognizer()
+
+        self.service = self.create_service(
+            StringCommand, '/speech_service/wake_up', self.command_callback)
+
+        self.lang = 'en'
+        self.mp3 = Mpg123()
+        self.out = Out123()
+
+    def command_callback(self, request, response):
+
+        self.synthesis("I'm ready.")
+
+        text = ''
+        while text == '':
+            text = self.recognition()
+
+        self.synthesis(text)
+
+        response.answer = text
+        return response
+
+    def recognition(self):
+        text = ''
+
+        with sr.Microphone() as source:
+            while text == '':
+                audio_data = self.init_rec.record(source, duration=5)
+                self.get_logger().info(f'音声認識を行います')
+
+                try:
+                    text = self.init_rec.recognize_google(audio_data)
+                except sr.UnknownValueError:
+                    pass
+
+        self.get_logger().info(f'認識したテキストは "{text}" です')
+
+        return text
+
+    def synthesis(self, text):
+
+        self.get_logger().info(f'音声合成を実行します')
+        self.get_logger().info(f'発話内容は "{text}"')
+
+        tts = gTTS(text, lang=self.lang[:2])
+        fp = BytesIO()
+        tts.write_to_fp(fp)
+        fp.seek(0)
+        self.mp3.feed(fp.read())
+
+        for frame in self.mp3.iter_frames(self.out.start):
+            self.out.play(frame)
+
 
 def main():
     rclpy.init()
-    node = StateMachine()
-    node.execute()
 
-if __name__ == '__main__':
-    main()
+    speech_service = SpeechService()
 
+    try:
+        rclpy.spin(speech_service)
+    except KeyboardInterrupt:
+        pass
 
-"""一時的に出力をここに記述
-[ DEBUG ] : Adding state (SEARCH, <__main__.Search object at 0x7fe01c483df0>, {'succeeded': 'GRASP', 'finished': 'end'})
-[ DEBUG ] : Adding state 'SEARCH' to the state machine.
-[ DEBUG ] : State 'SEARCH' is missing transitions: {}
-[ DEBUG ] : TRANSITIONS FOR SEARCH: {'succeeded': 'GRASP', 'finished': 'end'}
-[ DEBUG ] : Adding state (GRASP, <__main__.Grasp object at 0x7fe01c483e80>, {'succeeded': 'EAT', 'failed': 'GRASP'})
-[ DEBUG ] : Adding state 'GRASP' to the state machine.
-[ DEBUG ] : State 'GRASP' is missing transitions: {}
-[ DEBUG ] : TRANSITIONS FOR GRASP: {'succeeded': 'EAT', 'failed': 'GRASP'}
-[ DEBUG ] : Adding state (EAT, <__main__.Eat object at 0x7fe01c483b80>, {'done': 'SEARCH'})
-[ DEBUG ] : Adding state 'EAT' to the state machine.
-[ DEBUG ] : State 'EAT' is missing transitions: {}
-[ DEBUG ] : TRANSITIONS FOR EAT: {'done': 'SEARCH'}
-[  INFO ] : State machine starting in initial state 'SEARCH' with userdata: 
-	[]
-[INFO] [1645967679.654245847] [state_machine]: I am searching.
-[INFO] [1645967679.654493238] [state_machine]: Got a sweet.
-[  INFO ] : State machine transitioning 'SEARCH':'succeeded'-->'GRASP'
-[INFO] [1645967679.654732177] [state_machine]: I graspped a food.
-[  INFO ] : State machine transitioning 'GRASP':'failed'-->'GRASP'
-[INFO] [1645967679.654967337] [state_machine]: I graspped a food.
-[  INFO ] : State machine transitioning 'GRASP':'failed'-->'GRASP'
-[INFO] [1645967679.655198608] [state_machine]: I graspped a food.
-[  INFO ] : State machine transitioning 'GRASP':'failed'-->'GRASP'
-[INFO] [1645967679.655429608] [state_machine]: I graspped a food.
-[  INFO ] : State machine transitioning 'GRASP':'succeeded'-->'EAT'
-[INFO] [1645967679.655659035] [state_machine]: I am eating.
-[  INFO ] : State machine transitioning 'EAT':'done'-->'SEARCH'
-[INFO] [1645967679.655911782] [state_machine]: I am searching.
-[INFO] [1645967679.656131619] [state_machine]: Got a sweet.
-[  INFO ] : State machine transitioning 'SEARCH':'succeeded'-->'GRASP'
-[INFO] [1645967679.656376124] [state_machine]: I graspped a food.
-[  INFO ] : State machine transitioning 'GRASP':'succeeded'-->'EAT'
-[INFO] [1645967679.656607392] [state_machine]: I am eating.
-[  INFO ] : State machine transitioning 'EAT':'done'-->'SEARCH'
-[INFO] [1645967679.656835255] [state_machine]: I am searching.
-[INFO] [1645967679.657070629] [state_machine]: Got a sweet.
-[  INFO ] : State machine transitioning 'SEARCH':'succeeded'-->'GRASP'
-[INFO] [1645967679.657318254] [state_machine]: I graspped a food.
-[  INFO ] : State machine transitioning 'GRASP':'succeeded'-->'EAT'
-[INFO] [1645967679.657550730] [state_machine]: I am eating.
-[  INFO ] : State machine transitioning 'EAT':'done'-->'SEARCH'
-[INFO] [1645967679.657780871] [state_machine]: I am searching.
-[INFO] [1645967679.658012732] [state_machine]: I am full.
-[  INFO ] : State machine terminating 'SEARCH':'finished':'end'
-[INFO] [1645967679.658260258] [state_machine]: outcom: end
-"""
+    rclpy.shutdown()
